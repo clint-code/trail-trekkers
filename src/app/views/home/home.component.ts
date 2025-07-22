3aa34b9a015f00a428ebd6246810490f141e69c6
Clinton Mutinda <cmutinda@mfs.co.ke>
2025-07-22 16:44:03 +0300

    Styled the footer and added draggable effect to icons

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from 'src/app/components/footer/footer.component';
+import { gsap } from 'gsap';
+import { Draggable } from 'gsap/Draggable';
+
+gsap.registerPlugin(Draggable);
 
 @Component({
   selector: 'app-home',
@@ -12,6 +16,20 @@ import { FooterComponent } from 'src/app/components/footer/footer.component';
     FooterComponent
   ]
 })
-export class HomeComponent {
+
+export class HomeComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+
+    const items = gsap.utils.toArray('.draggable-item') as HTMLElement[];
+
+    Draggable.create(items, {
+      type: 'x,y',
+      bounds: ".content-left",
+      inertia: true
+    });
+
+
+  }
 
 }
