b172517ebaa7f46a9c8ef3a36a4e3b6edaadacfe
Clinton Mutinda <cmutinda@mfs.co.ke>
2025-07-22 12:02:25 +0300

    Added font family to tailwind config file and set up the header component

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ]
 })
 export class HomeComponent {
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+        garamondRegular: ['"apple_garamondregular"', 'serif'],
+        garamondBold: ['"apple_garamondbold"', 'serif'],
+        garamondItalic: ['"apple_garamonditalic"', 'serif'],
+        passion: ['"passion_oneregular"', 'sans-serif'],
+        speakeasy: ['"sf-speakeasy"', 'sans-serif'],
+    },
   },
   plugins: [],
 }
