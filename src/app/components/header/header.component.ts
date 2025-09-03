92084a867471e41c57862e193b1be5de0d11a15f
clint-code <mutinda1993@gmail.com>
2025-09-03 12:26:20 +0300

    Added sidebar mobile menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule, DOCUMENT } from '@angular/common';
+
+import { FaIconLibrary, FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { faCompass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +11,34 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./header.component.scss'],
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    CommonModule,
+    FaIconComponent
   ],
 
 })
 export class HeaderComponent {
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private library: FaIconLibrary
+  ) {
+    this.library.addIcons(
+      faCompass
+    );
+  }
+
+  isMobileMenuOpen = false;
+
+  toggleMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+
+    // const menu = this.document.getElementById('side-menu');
+
+    // if (menu) {
+    //   menu.classList.toggle('-translate-x-full');
+    // }
+
+  }
 
 }
