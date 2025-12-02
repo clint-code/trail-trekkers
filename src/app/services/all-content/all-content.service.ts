7b25b782f16a4ecc99b466b38822e7fff98e12d0
clint-code <mutinda1993@gmail.com>
2025-12-02 10:17:25 +0300

    Added Wordpress API

--- src/app/services/all-content/all-content.service.ts
+++ src/app/services/all-content/all-content.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../../environments/environment.prod';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -27,6 +29,12 @@ export class AllContentService {
 
   getHikePosts(): Observable<any[]> {
     return this.http.get<any[]>("/assets/data/hike-posts.json");
+  }
+
+  getAllUpcomingHikes(): Observable<any[]> {
+
+    return this.http.get<any[]>(`${environment.contentRoot}upcoming_hikes`);
+
   }
 
 }
